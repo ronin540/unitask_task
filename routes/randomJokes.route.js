38a4c621bff5774b1c15d4ea0540dd97c38a3544
ronin540 <satyamor2@gmail.com>
2023-06-15 18:39:28 +0530

    optional chaining is added in "random-joke" api

--- routes/randomJokes.route.js
+++ routes/randomJokes.route.js
@@ -6,6 +6,7 @@ const { verifyUser } = require("../middlewares/authentication");
 router.get("/api/random-joke", verifyUser, async (req, res, next) => {
   try {
     let joke = await axios.get("https://api.chucknorris.io/jokes/random");
+    joke.data.value = joke?.data?.value?.split(" ")?.reverse()?.join(" ");
     res.send(joke.data);
   } catch (error) {
     next(error);
